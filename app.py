8c0560140df1882ef0c5e94a5d464f2cf002e641
Bhavika Arage <85723109+BhauArage@users.noreply.github.com>
2022-10-30 12:48:34 +0530

    changes made

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from multiprocessing.spawn import prepare
+from turtle import title
 from flask import Flask, render_template, request, redirect, url_for, session
 import ibm_db
 import re
@@ -21,67 +23,26 @@ conn = ibm_db.connect(
 @app.route("/", methods=["POST", "GET"])
 @app.route("/home")
 def home():
-    return render_template("home.html", title="Home", msg=" hiṇ ")
-
-
-##this works
-@app.route("/sample", methods=["POST", "GET"])
-def sample():
-    if request.method == "POST":
-        amount = request.form["amount"]
-        sql = "INSERT INTO BUDGETS(id,maxbudget) VALUES(?,?)"
-        stmt = ibm_db.prepare(conn, sql)
-        ibm_db.bind_param(stmt, 1, 100)
-        ibm_db.bind_param(stmt, 2, amount)
-        ibm_db.execute(stmt)
-        return redirect(request.referrer)
-    sql = "SELECT * FROM budgets WHERE id =100"
-    stmt = ibm_db.prepare(conn, sql)
-    ibm_db.execute(stmt)
-    account = ibm_db.fetch_assoc(stmt)
-
-    return render_template(
-        "sample.html", title="Sample", account=account, messa="<h1>BHAVIKA</h1>"
-    )
-
-
-@app.route("/dashboard")
-def dashboard():
-    sql = "SELECT * FROM clients WHERE id =?"
-    stmt = ibm_db.prepare(conn, sql)
-    ibm_db.bind_param(stmt, 1, session["id"])
-    ibm_db.execute(stmt)
-    account = ibm_db.fetch_assoc(stmt)
-    return render_template("dashboard.html", title="Dashboard", account=account)
-
-
-@app.route("/logout")
-def logout():
-    session.pop("Loggedin", None)
-    session.pop("id", None)
-    session.pop("username", None)
-    return redirect("/")
+    return render_template("home.html", title="Home")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     global userid
     msg = " "
-    print("heyoo")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        print("heyoo")
         sql = "SELECT * FROM clients WHERE username =? AND password =?"
         stmt = ibm_db.prepare(conn, sql)
         ibm_db.bind_param(stmt, 1, username)
         ibm_db.bind_param(stmt, 2, password)
         ibm_db.execute(stmt)
         account = ibm_db.fetch_assoc(stmt)
-        print(account)
         if account:
             session["Loggedin"] = True
             session["id"] = account["ID"]
+            session["email"] = account["EMAIL"]
             userid = account["ID"]
             session["username"] = account["USERNAME"]
             return redirect("/dashboard")
@@ -121,6 +82,82 @@ def register():
             ibm_db.execute(stmt)
             return redirect("/login")
     return render_template("register.html", msg=msg, title="Register")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("Loggedin", None)
+    session.pop("id", None)
+    session.pop("username", None)
+    return redirect("/")
+
+
+@app.route("/dashboard")
+def dashboard():
+    sql = "SELECT * FROM clients WHERE id =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt, 1, session["id"])
+    ibm_db.execute(stmt)
+    return render_template("dashboard.html", title="Dashboard")
+
+
+@app.route("/changePassword/", methods=["POST", "GET"])
+def changePassword():
+    msg = "Enter the new password"
+    if request.method == "POST":
+        pass1 = request.form["pass1"]
+        pass2 = request.form["pass2"]
+        if pass1 == pass2:
+            sql = "UPDATE CLIENTS SET password=? where id=?"
+            stmt = ibm_db.prepare(conn, sql)
+            ibm_db.bind_param(stmt, 1, pass1)
+            ibm_db.bind_param(stmt, 2, userid)
+            if ibm_db.execute(stmt):
+                msg = "Successfully Changed Password!!!!"
+
+        else:
+            msg = "Passwords not equal"
+    return render_template("dashboard.html", msg=msg, title="DashBoard")
+
+
+@app.route("/changeBudget/", methods=["POST", "GET"])
+def changeBudget():
+    msg = "Enter the new budget"
+    if request.method == "POST":
+        budgetAmount = request.form["budgetAmount"]
+        sql = "UPDATE BUDGETS SET password=? where id=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, budgetAmount)
+        ibm_db.bind_param(stmt, 2, userid)
+        if ibm_db.execute(stmt):
+            msg = "Successfully Changed Budget!!!!"
+        else:
+            msg = "Budget not changed"
+    return render_template("dashboard.html", msg=msg, title="DashBoard")
+
+
+@app.route("/addBudget")
+def addBudget():
+    pass
+
+
+@app.route("/log_today")
+def logToday():
+    pass
+
+
+@app.route("/addExpense/")
+def addExpense():
+    pass
+
+
+@app.route("/addIncome/")
+def addIncome():
+    pass
+
+
+# @app.route("/Edit")
+###Visualization functions
 
 
 if __name__ == "__main__":
